565a004e19763fd3881be40e3b406b41a2d2905e
B22-167-2025 <B22-167-2025@edu-il.it>
2025-07-10 14:32:15 +0200

    Save

---
+++ appReact01/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return (
+        <>
+            <ul>
+                <li>Pag 1</li>
+                <li>Pag 2</li>
+                <li>Pag 3</li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar;
