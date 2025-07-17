89e33285824cf7c56056149f1ef4d722f1ce1662
ionv95 <ion.vacula@edu.immaginazioneelavoro.it>
2025-07-17 13:49:29 +0200

    Save

--- appReact02/components/Card.jsx
+++ appReact02/components/Card.jsx
@@ -1,13 +1,38 @@
 import './Card.css'
 
-function Card(){
+// function Card(){
     
-    const title = "Tucano";
-    const imgUrl = "https://images.unsplash.com/photo-1550853024-fae8cd4be47f?q=80&w=2088&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-    const description = "Questo è un Tucano";
+//     const title = "Tucano";
+//     const imgUrl = "https://images.unsplash.com/photo-1550853024-fae8cd4be47f?q=80&w=2088&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+//     const description = "Questo è un Tucano";
+
+//     return (
+//         <div className="card">
+//             <div className="card-image">
+//                 <img src={imgUrl} alt="" />
+//             </div>
+
+//             <div>
+//                 <h2>
+//                     {title}
+//                 </h2>
+//                 <p>
+//                     {description}
+//                 </p>
+//             </div>
+//         </div>
+//     )
+// }
+
+//In questo modo definisco delle properties che verranno valorizzate nel component Parent.
+//Il dato esiste nel Parent.
+function Card(props){
+    const title = props.title;
+    const description = props.description;
+    const imgUrl = props.imgUrl;
 
     return (
-        <div className="card">
+            <div className="card">
             <div className="card-image">
                 <img src={imgUrl} alt="" />
             </div>
@@ -21,6 +46,7 @@ function Card(){
                 </p>
             </div>
         </div>
+
     )
 }
 
--- appReact02/src/App.jsx
+++ appReact02/src/App.jsx
@@ -25,7 +25,20 @@ function App() {
       <hr />
 
       <div className='card-container'>
-        <Card></Card>
+        <Card title = "Tucano"
+              imgUrl = "https://images.unsplash.com/photo-1550853024-fae8cd4be47f?q=80&w=2088&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              description = "Are you Toucan to me?">
+        </Card>
+
+        <Card title = "Flamingo"
+              imgUrl = "https://images.unsplash.com/photo-1497206365907-f5e630693df0?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              description = "Are you Flamingo to me?">
+        </Card>
+
+        <Card title = "Parrot"
+              imgUrl = "https://images.unsplash.com/photo-1552728089-57bdde30beb3?q=80&w=725&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              description = "Are you Parrot to me?">
+        </Card>
       </div>
     </>
   )
